f86cf2283fdddd526bbb7d1620a009a6159538f0
nebula002 <ananya.agra02@gmail.com>
2024-09-17 22:13:53 +0530

    Navbar, Footer and Our vision section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,15 @@ import Advantage from './Advantage'
 import logo from "../assets/e-lit01.jpg"
 import FAQ from './FAQ'
 import Stat from './Stat'
+import Navbar from './NavBar'
+import OurVision from './OurVision'
+import Footer from './Footer'
 
 
 const Home = () => {
   return (<>
-    <div className="bg-[url('assets/photo0.jpeg')] bg-no-repeat bg-cover ">
+  <Navbar/>
+    <div className="bg-[url('assets/HomeBG.webp')] bg-no-repeat bg-cover ">
 <div className='flex justify-center items-center h-screen'>
 
 <div className=''>
@@ -17,15 +21,20 @@ const Home = () => {
 <img src={logo} className='w-[25vh] h-[25vh]'/></h1>
 </div>
 <div className='flex justify-center p-'> 
-<h1 className='text-white text-2xl font-mono px-5'>The World's First Effective Electric Lighter</h1></div>
+<h1 className='text-white text-4xl font-bold font-mono px-5'>The World's First Effective Electric Lighter</h1></div>
    <div className='flex justify-center p-5'>  <button className='bg-orange-500 hover:bg-orange-700 text-white font-bold py-3 px-8 rounded-full'>SHOP NOW</button></div> 
 </div>
 </div>
     </div>
+    
+<OurVision/>
 <Advantage/>
 <FAQ/>
 <Stat/>
 <Testimonial/>
+<Footer/>
+
+
 </>
   )
 }
---
+++ src/components/OurVision.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Fire from '../assets/fireflames.jpg'
+
+const OurVision = () => {
+  return (
+    <div className="max-w-screen-xl h-svh bg-[#fdfdf8] flex-col justify-center items-start inline-flex z-0 ">
+        <div className='w-screen h-1/2 z-10 relative flex flex-col items-center justify-center '>
+           <img className=' w-full absolute inset-y-0 right-0' src={Fire} alt=''/>
+            {/*Vision Container*/ }
+            <div className='z-20 w-3/4 bg-white/60 backdrop-blur-[15.6px] border-2 border-black rounded-lg p-8 mt-8'>
+              <div className="w-full mt-7 text-black font-bold text-4xl font-['Atkinson Hyperlegible'] text-center ">
+              Our Vision
+              </div>
+              <div className="w-full h-3/5  mt-12  text-center text-black text-2xl font-normal font-['Atkinson Hyperlegible'] ">
+              At e-lit, our vision is to replace the traditional disposable lighter, which contributes to pollution and waste, with a sustainable, rechargeable alternative.
+             <br></br>
+             The e-lit electric lighter combines practicality with eco-friendliness, using biodegradable PVC and a rechargeable lithium-ion battery to replace harmful, disposable butane lighters.
+              </div>
+           </div>  
+        </div>   
+
+        
+        
+        <div className='w-screen h-screen mt-12 z-10 relative flex flex-col items-center justify-center'>
+          {/*Advantage Container */}
+          <div className='z-20 w-3/4 bg-white/60 backdrop-blur-[15.6px] border-2 border-black rounded-lg p-8'>
+              <div className="w-full mt-7 text-black font-bold text-4xl font-['Atkinson Hyperlegible'] text-center ">
+              Why choose E-Lit?
+              </div>
+              <div className="w-full h-3/5  mt-12  text-center text-black text-2xl font-normal font-['Atkinson Hyperlegible'] ">
+              Choose E-lit for its eco-friendly design and sustainability, featuring biodegradable materials and a rechargeable battery. Enjoy a cost-effective and durable solution that reduces waste and saves you money over time.
+              </div>
+           </div> 
+        </div>
+    </div>
+  )
+}
+
+export default OurVision
